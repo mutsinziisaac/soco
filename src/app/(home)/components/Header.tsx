286c9cd67e9b57364e3d20df69f56786db1575e2
mutsinzi isaac <mutsinziisaac123@gmail.com>
2025-02-24 01:50:30 +0300

    fix-ts-errors

--- src/app/(home)/components/Header.tsx
+++ src/app/(home)/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="bg-white shadow-sm">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="w-full py-6 flex items-center justify-between">
+        <div className="w-full py-2 flex items-center justify-between">
           <div className="flex items-center">
             <Link href="/" className="flex items-center">
               <Image
